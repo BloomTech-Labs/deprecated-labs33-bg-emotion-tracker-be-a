424e5edf3e8652c3ae699a2545b0be115246c8fa
Mike <mgalvin@tribevr.io>
2021-04-12 12:27:40 -0500

    adds club controller, club service, club service impl, and club repo

---
+++ src/main/java/com/lambdaschool/oktafoundation/services/ClubServiceImpl.java
@@ -0,0 +1,10 @@
+package com.lambdaschool.oktafoundation.services;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@Service(value = "clubService")
+public class ClubServiceImpl implements ClubService
+{
+}
