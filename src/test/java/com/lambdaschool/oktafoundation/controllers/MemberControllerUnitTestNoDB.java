b1172d066ed1663d4bd428e4d34a2131ddcb3564
Mike <mgalvin@tribevr.io>
2021-04-14 11:14:36 -0500

    adds listallmembers controller unit no DB test

--- src/test/java/com/lambdaschool/oktafoundation/controllers/MemberControllerUnitTestNoDB.java
+++ src/test/java/com/lambdaschool/oktafoundation/controllers/MemberControllerUnitTestNoDB.java
@@ -1,5 +1,4 @@
 package com.lambdaschool.oktafoundation.controllers;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.lambdaschool.oktafoundation.OktaFoundationApplicationTest;
 import com.lambdaschool.oktafoundation.models.*;
@@ -22,16 +21,13 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MockMvcBuilder;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-
 import java.util.ArrayList;
 import java.util.List;
-
 import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
@@ -43,46 +39,28 @@ import static org.junit.Assert.*;
 @WithMockUser(username = "llama001@maildrop.cc",
         roles = {"SUPERADMIN"})
 public class MemberControllerUnitTestNoDB {
-
     @Autowired
     private WebApplicationContext webApplicationContext;
-
     private MockMvc mockMvc;
-
     @MockBean
     private MemberService memberService;
-
     @MockBean
     private MemberRepository memberRepository;
-
     @MockBean
     private UserService userService;
-
     private List<Member> memberList;
-
     @MockBean
     private UserRepository userrepos;
-
     private List<User> userList;
-
     private User u1;
-
-
     @Before
     public void setUp() throws Exception
     {
-
         memberList = new ArrayList<>();
         userList = new ArrayList<>();
 
         Role r1 = new Role("superadmin");
         r1.setRoleid(1);
-        Role r2 = new Role("clubdir");
-        r2.setRoleid(2);
-        Role r3 = new Role("ydp");
-        r3.setRoleid(3);
-        Role r4 = new Role("user");
-        r3.setRoleid(4);
 
         // super admin
         u1 = new User("llama001@maildrop.cc");
@@ -100,78 +78,28 @@ public class MemberControllerUnitTestNoDB {
         u1.setUserid(101);
         userList.add(u1);
 
-        // club director
-        User u2 = new User("llama002@maildrop.cc");
-        u2.getRoles()
-                .add(new UserRoles(u2,
-                        r2));
-        u2.getUseremails()
-                .add(new Useremail(u2,
-                        "llama002@maildrop.cc"));
-        u2.getUseremails()
-                .get(0)
-                .setUseremailid(20);
-
-        u2.setUserid(102);
-        userList.add(u2);
-
-        // youth development professional ydp
-        User u5 = new User("llama005@maildrop.cc");
-        u5.getRoles()
-                .add(new UserRoles(u5,
-                        r3));
-
-        u5.getUseremails()
-                .add(new Useremail(u5,
-                        "llama005@maildrop.cc"));
-        u5.getUseremails()
-                .get(0)
-                .setUseremailid(50);
-
-        u5.setUserid(105);
-        userList.add(u5);
-
-        // user
-        User u7 = new User("llama007@maildrop.cc");
-        u7.getRoles()
-                .add(new UserRoles(u7,
-                        r4));
-
-        u7.setUserid(107);
-        userList.add(u7);
-
-
-        System.out.println("\n*** Seed Data ***");
-        for (User u : userList)
-        {
-            System.out.println(u.getUserid() + " " + u.getUsername());
-        }
-        System.out.println("*** Seed Data ***\n");
-
-
         Member mem1 = new Member(1, "Test001");
         Member mem2 = new Member(2, "Test002");
         Member mem3 = new Member(3, "Test003");
-
         memberList.add(mem1);
         memberList.add(mem2);
         memberList.add(mem3);
 
         RestAssuredMockMvc.webAppContextSetup(webApplicationContext);
-
         mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
                 .apply(SecurityMockMvcConfigurers.springSecurity())
                 .build();
     }
-
     @After
     public void tearDown() throws Exception {
     }
-
     @Test
     public void listAllMembers()  throws  Exception
     {
         String apiUrl = "/members/members";
+        Mockito.when(userrepos.findByUsername(u1.getUsername()))
+                .thenReturn(u1);
+
         Mockito.when(memberService.findAll())
                 .thenReturn(memberList);
 
@@ -179,17 +107,14 @@ public class MemberControllerUnitTestNoDB {
                 .accept(MediaType.APPLICATION_JSON);
         MvcResult r = mockMvc.perform(rb)
                 .andReturn();
-        String tr = r.getResponse()
-                .getContentAsString();
-
+        String tr = r.getResponse().getContentAsString();
+//        System.out.println(tr);
         ObjectMapper mapper = new ObjectMapper();
         String er = mapper.writeValueAsString(memberList);
+//        System.out.println(er);
 
-        System.out.println(er);
         assertEquals(er, tr);
-
     }
-
     @Test
     public void uploadSimple() {
     }
