5d863d0aa7547abd417fca7bb43db593b04f8064
ginabethrussell <ginabeth.russell@gmail.com>
2021-04-13 20:35:58 -0400

    adds saveNewMembers method to MemberService interface

--- src/main/java/com/lambdaschool/oktafoundation/services/MemberService.java
+++ src/main/java/com/lambdaschool/oktafoundation/services/MemberService.java
@@ -1,7 +1,10 @@
 package com.lambdaschool.oktafoundation.services;
 
+import com.github.javafaker.Bool;
 import com.lambdaschool.oktafoundation.models.Member;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 public interface MemberService
@@ -9,4 +12,7 @@ public interface MemberService
     List<Member> findAll();
 
     Member save(Member member);
+
+    List<Member> saveNewMembers(InputStream stream) throws IOException;
+
 }
