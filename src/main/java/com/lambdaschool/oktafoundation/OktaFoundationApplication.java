c2101a94d0bdc6f8f57dac7330fd6898f0853947
ginabethrussell <ginabeth.russell@gmail.com>
2021-04-13 20:27:00 -0400

    removes local postgres config properties file from the main branch

--- src/main/java/com/lambdaschool/oktafoundation/OktaFoundationApplication.java
+++ src/main/java/com/lambdaschool/oktafoundation/OktaFoundationApplication.java
@@ -12,8 +12,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
  */
 @EnableJpaAuditing
 @SpringBootApplication
-
-@PropertySource(value = "file:/Users/ginabethrussell/oktajavaconf.properties", ignoreResourceNotFound = true)
 public class OktaFoundationApplication
 {
     /**
