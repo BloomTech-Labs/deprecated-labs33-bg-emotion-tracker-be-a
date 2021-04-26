1e24fd17eed59f44ceb76b6a61a6f688378d21f4
justin-mavity <justinmavity1021@gmail.com>
2021-04-26 13:13:15 -0500

    Wrote a post mapping to implement that csv file uploader

--- src/main/java/com/lambdaschool/oktafoundation/controllers/ClubController.java
+++ src/main/java/com/lambdaschool/oktafoundation/controllers/ClubController.java
@@ -1,6 +1,7 @@
 package com.lambdaschool.oktafoundation.controllers;
 
 import com.lambdaschool.oktafoundation.models.Club;
+import com.lambdaschool.oktafoundation.models.Program;
 import com.lambdaschool.oktafoundation.repository.ClubRepository;
 import com.lambdaschool.oktafoundation.services.ClubService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,8 +9,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -29,6 +32,17 @@ public class ClubController
     {
         List<Club> myClubs = clubService.findAll();
         return new ResponseEntity<>(myClubs, HttpStatus.OK);
+    }
+
+    @PostMapping (value = "/upload", consumes = "multipart/form-data", produces = "application/json")
+    public ResponseEntity<?> uploadClubs(
+        MultipartFile csvfile) throws Exception {
+        List<Club> addedClubs = clubService.saveNewClubs(csvfile.getInputStream());
+        if (addedClubs.size() < 1) {
+            return new ResponseEntity<>(HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(addedClubs, HttpStatus.CREATED);
+        }
     }
 
 }
