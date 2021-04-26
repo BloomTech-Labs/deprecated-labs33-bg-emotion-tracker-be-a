890506166a507c2e95e7b52656d744dcbb8c5cad
justin-mavity <justinmavity1021@gmail.com>
2021-04-26 13:12:13 -0500

    Implemented a saveNewClubs function that would implement Giana's csv file uploader

--- src/main/java/com/lambdaschool/oktafoundation/services/ClubService.java
+++ src/main/java/com/lambdaschool/oktafoundation/services/ClubService.java
@@ -2,6 +2,8 @@ package com.lambdaschool.oktafoundation.services;
 
 import com.lambdaschool.oktafoundation.models.Club;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 public interface ClubService
@@ -9,6 +11,8 @@ public interface ClubService
     List<Club> findAll();
 
     Club save(Club club);
+
+    List<Club> saveNewClubs(InputStream stream) throws IOException;
 
     public void deleteAll();
 }
--- src/main/java/com/lambdaschool/oktafoundation/services/ClubServiceImpl.java
+++ src/main/java/com/lambdaschool/oktafoundation/services/ClubServiceImpl.java
@@ -1,15 +1,22 @@
 package com.lambdaschool.oktafoundation.services;
 
+import com.lambdaschool.oktafoundation.exceptions.ResourceFoundException;
 import com.lambdaschool.oktafoundation.exceptions.ResourceNotFoundException;
 import com.lambdaschool.oktafoundation.models.Club;
 import com.lambdaschool.oktafoundation.models.ClubPrograms;
 import com.lambdaschool.oktafoundation.models.Program;
 import com.lambdaschool.oktafoundation.repository.ClubRepository;
+import com.lambdaschool.oktafoundation.repository.ProgramRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Transactional
@@ -21,6 +28,10 @@ public class ClubServiceImpl implements ClubService
 
     @Autowired
     ProgramService programService;
+
+    private ClubRepository clubrepos;
+
+    private ProgramRepository programrepos;
 
     @Override
     public List<Club> findAll()
@@ -35,33 +46,100 @@ public class ClubServiceImpl implements ClubService
 
     @Transactional
     @Override
-    public Club save(Club club)
-    {
-        Club newClub = new Club();
-        if (club.getClubid() != 0)
+    public Club save(Club club) {
+        if (club.getPrograms()
+            .size() > 0)
         {
-            clubRepository.findById(club.getClubid())
-                .orElseThrow(() -> new ResourceNotFoundException("Club id " + club.getClubid() + " not found!"));
-            newClub.setClubid(club.getClubid());
+            throw new ResourceFoundException("Club Programs are not updated through Program.");
         }
 
-        newClub.setClubname(club.getClubname());
-        newClub.setClubdirector(club.getClubdirector());
-
-        newClub.getPrograms().clear();
-        for (ClubPrograms cp : club.getPrograms())
-        {
-            Program addProgram = programService.findProgramById(cp.getProgram().getProgramid());
-            newClub.getPrograms().add(new ClubPrograms(newClub, addProgram));
-        }
-
-        return clubRepository.save(newClub);
+        return clubrepos.save(club);
     }
 
     @Override
     public void deleteAll()
     {
         clubRepository.deleteAll();
+    }
+
+    @Override
+    public List<Club> saveNewClubs(InputStream stream) throws IOException
+    {
+        List<Club> addedClubs = new ArrayList<>();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+        String programname;
+        String clubname;
+        String row;
+        // removes header line from CSV file
+        String headerLine = reader.readLine();
+        String[] columnheaders = headerLine.split(",");
+        List<String> columnheaderlist = Arrays.asList(columnheaders);
+        ArrayList<String> listofheaders = new ArrayList<>(columnheaderlist);
+
+        // Validate two columns in headers
+        if(listofheaders.size() != 2){
+            throw new ResourceNotFoundException("CSV file must contain Program Name and Club headers");
+        }
+
+        // Validate correct headers
+        if (!(listofheaders.get(0).equalsIgnoreCase("Club Name") && listofheaders.get(1).equalsIgnoreCase("Program")))
+        {
+            throw new ResourceNotFoundException("CSV file must contain CLub Name and Program headers, file contains " + listofheaders.get(0) + " and " + listofheaders.get(1));
+        }
+
+        while((row = reader.readLine())!= null)
+        {
+            String[] rowarray = row.split(",");
+            List<String> rowarraylist = Arrays.asList(rowarray);
+            List<String> fields = new ArrayList<>(rowarraylist);
+
+            programname = fields.get(1).toLowerCase();
+            programname = programname.replaceAll("^\"|\"$",
+                "");
+            clubname = fields.get(0).toLowerCase();
+            clubname = clubname.replaceAll("^\"|\"$",
+                "");
+
+            // if club, doesn't exist throw resource not found
+            // clubs are not updated through programs
+            Program p = programrepos.findByNameIgnoreCase(programname);
+            if (p == null)
+            {
+                throw new ResourceNotFoundException("Program " + programname + " not found");
+            }
+
+            // if program doesn't exist, create it
+            Club c = clubrepos.findByClubnameIgnoreCase(clubname);
+            if (c == null)
+            {
+                c = clubrepos.save(new Club(clubname));
+
+            }
+
+            // verify program is not already associated with this club
+            // loop through all the programs clubs
+            // if any clubprogram has the clubname being added
+            // set the boolean flag to false
+            // if after the loop, boolean is true
+            // add the new relationship
+            boolean isNewClubProgram = true;
+            for (ClubPrograms cp : c.getPrograms())
+            {
+                if(cp.getProgram().getName().equalsIgnoreCase(p.getName())){
+                    isNewClubProgram = false;
+                }
+            }
+
+            if (isNewClubProgram)
+            {
+                c.getPrograms()
+                    .add(new ClubPrograms(c,
+                        p));
+                addedClubs.add(c);
+            }
+
+        }
+        return addedClubs;
     }
 
 }
